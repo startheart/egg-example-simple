3ed09998760013da4c9e14595e055d7a60ec11fc
sgoddon <xuguodong@didichuxing.com>
2018-02-06 12:13:04 +0800

    init

---
+++ config/config.default.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1517837016049_8722';
+
+  // add your config here
+  config.middleware = [
+    'robot'
+  ];
+
+  // robot's configurations
+  config.robot = {
+    ua: [
+      /Baiduspider/i,
+    ]
+  };
+
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+
+  // 添加 news 的配置项
+  config.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  };
+
+  return config;
+};
