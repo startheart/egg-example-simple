6e51561632600ac0b53dd890fa7c5c818873be46
sgoddon <xuguodong@didichuxing.com>
2018-02-06 14:23:02 +0800

    update

--- app/controller/news.js
+++ app/controller/news.js
@@ -5,7 +5,11 @@ class NewsController extends Controller {
   async list() {
     const ctx = this.ctx;
     const page = ctx.query.page || 1;
-    const newsList = await ctx.service.news.list(page);
+    // const newsList = await ctx.service.news.list(page);
+    const newsList = [
+      { id: 1, title: 'this is news 1', url: '/news/1' },
+      { id: 2, title: 'this is news 2', url: '/news/2' }
+    ];
     await ctx.render('news/list.tpl', { list: newsList });
   }
 }
